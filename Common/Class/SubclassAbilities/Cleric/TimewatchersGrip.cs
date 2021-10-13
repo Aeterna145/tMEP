db1e4e007b424d5d9a0adb8432f81cee97351014
Æterna <67712622+Aeterna145@users.noreply.github.com>
2021-10-13 01:03:29 -0400

    added common folder

---
+++ Common/Class/SubclassAbilities/Cleric/TimewatchersGrip.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+
+namespace MEPMod.Common.Class.SubclassAbilities.Cleric
+{
+    public class TimewatchersGrip : AbilityType
+    {
+        public override void SetStaticDefaults(){
+            AbilityName = "Timewatcher's Grip";
+        }
+        public override void SetDefaults(){
+            AbilityCastCooldown = 1200;
+            AbilityEnergyCost = 100;
+        }
+        public override void Cast(Player player){
+            Main.NewText("Timewatcher's Grip Successfully Cast (This is test text and will not be used post v0.25)");
+        }
+    }
+}
---
+++ Common/Class/SubclassAbilities/Cleric/UniversalSoul.cs
@@ -0,0 +1,21 @@
+﻿using Terraria;
+
+namespace MEPMod.Common.Class.SubclassAbilities.Cleric
+{
+    public class UniversalSoul : AbilityType
+    {
+        public override void SetStaticDefaults(){
+            AbilityName = "Universal Soul";
+        }
+        public override void SetDefaults(){
+            AbilityCastCooldown = 120;
+            AbilityEnergyCost = 25;
+            IsToggle = true;
+            HasAltFunc = true;
+        }
+        public override void Cast(Player player)
+        {
+            Main.NewText("Universal Soul Successfully Cast (This is test text and will not be used post v0.25)");
+        }
+    }
+}
